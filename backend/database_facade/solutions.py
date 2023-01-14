17de83c5f3610ed6c5d57eaa57718ac463ddbc7b
Wojciech Barczynski <barczynski.w@gmail.com>
2023-01-14 22:01:50 +0100

    add user answer handling and db updating

--- backend/database_facade/solutions.py
+++ backend/database_facade/solutions.py
@@ -53,3 +53,20 @@ def get_solutions():
         
     return solutions_list
 
+def post_solution(user_id, task_id, work_time, user_answer, is_correct):
+    connection = pymysql.connect(
+        host="mysql.agh.edu.pl", 
+        user="swoznia2",
+        password="rMBsvPsTJQ8etui0",
+        database="swoznia2"
+        )
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO solutions (TaskID, UserID, WorkTimeSeconds, UserAnswer, IsCorrect)" \
+                + "\n" + "VALUES (%s, %s, %s, %s, %s)"
+            val = (f"{user_id}", f"{task_id}", f"{work_time}", f"{user_answer}", f"{is_correct}")
+            cursor.execute(sql, val)
+            connection.commit()
+    finally:
+        connection.close()
+        
---
+++ backend/rest_api/handle_answer.py
@@ -0,0 +1,33 @@
+import sys
+import os
+dir = os.path.dirname(__file__)
+filepath = os.path.join(dir, '../..')
+sys.path.insert(0, filepath)
+
+from backend.database_facade.users import get_users_df, update_user_rating
+from backend.database_facade.tasks import get_tasks_df, update_task_rating, get_solution_url
+from backend.database_facade.solutions import post_solution
+import math
+
+def handle_user_answer(user_id, task_id, user_answer, work_time):
+    users_df = get_users_df()
+    tasks_df = get_tasks_df()
+    correct_answer = float(tasks_df.loc[tasks_df['id'] == task_id].answer.values[0])
+    
+    is_correct = is_answer_correct(user_answer, correct_answer)
+    
+    (updated_user_rating, updated_task_rating, user_point_delta) = (1000, 1000, 10) #TODO
+
+    update_user_rating(user_id, updated_user_rating)
+    update_task_rating(user_id, updated_task_rating)
+    post_solution(user_id, task_id, work_time, user_answer, int(is_correct))
+    solution_url = get_solution_url(task_id)
+    
+    return (int(is_correct), correct_answer, solution_url, user_point_delta)
+    
+
+def is_answer_correct(user_answer, correct_answer):
+    return math.isclose(user_answer, correct_answer, rel_tol=0.001)
+    
+    
+    
