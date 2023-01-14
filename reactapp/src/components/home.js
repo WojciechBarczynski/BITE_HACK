c70b06eea132b0f7c37423e594c1c837aba57de3
szWozniak <szymek.woz@gmail.com>
2023-01-14 18:51:32 +0100

    :)

--- reactapp/src/components/home.js
+++ reactapp/src/components/home.js
@@ -15,6 +15,7 @@ const HomeComponent = () => {
 
     return (
         <div className='conteiner'>
+            <NavbarComponent/>
             <TaskComponent/>
             <ButtonsComponent/>
         </div>
--- reactapp/src/components/login.js
+++ reactapp/src/components/login.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { redirect, useNavigate } from 'react-router-dom';
 import loginService from '../services/loginService';
+import NavbarComponent from './navbar';
 
 
 const LoginComponent = () => {
@@ -24,10 +25,13 @@ const LoginComponent = () => {
     }
 
     return (
-        <div>
+        <div className='conteiner'>
+            <NavbarComponent/>
+            <div>
             <input type="text" value={loginInput} 
                 onChange={(e) => {setLoginInput(e.target.value)}} />
             <button onClick={onLogin}>Zaloguj się</button>
+        </div>
         </div>
     );
 };
