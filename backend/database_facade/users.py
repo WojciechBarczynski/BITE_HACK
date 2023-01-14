ab2e4a62425bf24405d837924b8e8a4297e54e38
Wojciech Barczynski <barczynski.w@gmail.com>
2023-01-14 17:15:56 +0100

    Sketch login

--- backend/database_facade/users.py
+++ backend/database_facade/users.py
@@ -1,6 +1,8 @@
 import pymysql
 import pandas as pd
 
+default_rating = 1000
+
 class TaskEntry:
     def __init__(self, id, name, rating):
         self.id = id
@@ -27,4 +29,25 @@ def get_users_df():
     
     return pd.DataFrame.from_records(user.__dict__ for user in users_list)
 
-print(get_users_df())
+
+def create_user(user_name):
+    users = get_users_df()
+    new_id = users['id'].max() + 1
+    
+    connection = pymysql.connect(
+        host="mysql.agh.edu.pl", 
+        user="swoznia2",
+        password="rMBsvPsTJQ8etui0",
+        database="swoznia2"
+        )
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO users (ID, Name, Rating)" + "\n" + "VALUES (%s, %s, %s)"
+            val = (f"{new_id}", f"{user_name}", f"{default_rating}")
+            cursor.execute(sql, val)
+            connection.commit()
+    finally:
+        connection.close()
+    return new_id
+    
+
