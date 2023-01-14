a855de3e98c863acf6b5ebe403bd6517edbbf298
szWozniak <szymek.woz@gmail.com>
2023-01-14 22:14:54 +0100

    answers

--- reactapp/src/components/task.js
+++ reactapp/src/components/task.js
@@ -1,16 +1,50 @@
 import React, { useState } from 'react';
+import answerService from '../services/answerService';
 import taskService from '../services/taskService';
 import '../style/task.css';
+
 const TaskComponent = () => {
 
     const [currentTask, setCurrentTask] = useState(null);
     const [taskImage, setTaskImage] = useState("");
+    const [answer, setAnswer] = useState("");
+    const [showingAnswer, setShowingAnswer] = useState(false);
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [ranking, setRanking] = useState(0);
 
     const generate = () => {
         taskService.generate().then((res) => {
             setCurrentTask(res.data.taskid)
             setTaskImage(res.data.taskurl)
+            setShowingAnswer(false)
         })
+    }
+
+    const handleAnswer = () => {
+        answerService.answer(localStorage.getItem("userid"), currentTask, answer)
+            .then(res => {
+                setShowingAnswer(true);
+                setTaskImage(res.data.link);
+                setAnswer(res.data.answer);
+                setIsCorrect(parseInt(res.data.result) == 0 ? false : true);
+                setRanking(res.data.pointdelta);
+            })
+    }
+
+    const getResultHeader = () => {
+        if(isCorrect) return <h2>Twoje rozwiązanie jest poprawne! (Ranking +{ranking})</h2>
+        else return <h2>Popełniłeś błąd (Ranking -{ranking*(-1)})</h2>
+    }
+
+    if(showingAnswer) {
+        return (
+            <div className='task'>
+                {getResultHeader()}
+                <img className="taskImage" src={taskImage} alt="Zdjęcie nie wczytało się poprawnie..." />
+                Poprawna odpowiedź: <b>{answer}</b><br />
+                <button onClick={generate} className="niceButton" style={{marginTop: "10px"}}>Przejdź do następnego zadania</button>
+            </div>
+        );
     }
 
     if(currentTask == null) {
@@ -26,8 +60,8 @@ const TaskComponent = () => {
         <div className='task'>
             <h2>Rozwiązujesz zadanie: #{currentTask}</h2>
             <img className="taskImage" src={taskImage} alt="Zdjęcie nie wczytało się poprawnie..." />
-            <input placeholder="Wpisz odpowiedź..." className="taskInput" type="text" /><br />
-            <button onClick={generate} className="niceButton" style={{marginTop: "10px"}}>Wyślij rozwiązanie</button>
+            <input placeholder="Wpisz odpowiedź..." className="taskInput" type="text" onChange={(e) => setAnswer(e.target.value)}/><br />
+            <button onClick={handleAnswer} className="niceButton" style={{marginTop: "10px"}}>Wyślij rozwiązanie</button>
         </div>
     );
 };
