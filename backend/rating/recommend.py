1c613dcd1a983335705a5a49f89108b1462fd449
Rusiek <szymon.rusiecki@protonmail.com>
2023-01-14 17:21:52 +0100

    recommend module

---
+++ backend/rating/recommend.py
@@ -0,0 +1,25 @@
+# ignore SettingWithCopyWarning
+import warnings
+from pandas.core.common import SettingWithCopyWarning
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
+class Recommend:
+    @staticmethod
+    def get_user_solutions(solutions_df, user_id):
+        records = solutions_df[solutions_df.user_id == user_id]
+        return records['id'].tolist()
+
+    @staticmethod
+    def get_recommendations(user_id, user_df, sol_df, task_df):
+        user_rating = user_df[user_df.id == user_id].rating.tolist()[0]
+        solved_tasks = sol_df[sol_df.user_id == user_id]
+        unsolved_tasks = task_df[~task_df.id.isin(solved_tasks.task_id)]
+        unsolved_tasks['rating'] = unsolved_tasks['rating'].apply(lambda x: x - user_rating)
+        unsolved_tasks['rating'] = unsolved_tasks['rating'].apply(lambda x: abs(x))
+        unsolved_tasks = unsolved_tasks.sort_values(by=['rating'])
+        return unsolved_tasks.head(1).id.tolist()
+        
+
+
+       
+
