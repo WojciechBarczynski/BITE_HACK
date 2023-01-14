f6be80673c1efd3e496e12b79ddb4ed4513f4ac9
Szymon Rusiecki <79122265+Rusiek@users.noreply.github.com>
2023-01-14 23:18:51 +0100

    linear transformation (#25)
    Co-authored-by: Rusiek <szymon.rusiecki@protonmail.com>

--- backend/rating/update.py
+++ backend/rating/update.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from backend.rating.trueSkills import TrueSkills
 
-FRONT_CONST = 60
+DB_CONST = 60
 
 class UpdateData:
     @staticmethod
@@ -15,10 +15,11 @@ class UpdateData:
         sol_df):
         time = 1
         expectedTime = 1
-        user_rating = user_df[user_df.id == user_id].rating.tolist()[0]
-        task_rating = task_df[task_df.id == task_id].rating.tolist()[0]
+        user_rating = user_df[user_df.id == user_id].rating.tolist()[0] / DB_CONST
+        task_rating = task_df[task_df.id == task_id].rating.tolist()[0] / DB_CONST
+
         new_user_rating = TrueSkills().update(user_rating, task_rating, is_ok, time, expectedTime)
         diff = new_user_rating - user_rating
         user_rating += diff
         task_rating -= diff
-        return int(FRONT_CONST * user_rating), int(FRONT_CONST * task_rating), int(FRONT_CONST * diff)
+        return int(DB_CONST * user_rating), int(DB_CONST * task_rating), int(DB_CONST * diff)
