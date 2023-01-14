bb02be120124c82b49bb155c37baca8c26193883
Rusiek <szymon.rusiecki@protonmail.com>
2023-01-15 00:47:45 +0100

    Math rework

--- backend/rating/update.py
+++ backend/rating/update.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from backend.rating.trueSkills import TrueSkills
+from trueSkills import TrueSkills
 
 DB_CONST = 60
 
@@ -19,7 +19,24 @@ class UpdateData:
         task_rating = task_df[task_df.id == task_id].rating.tolist()[0] / DB_CONST
 
         new_user_rating = TrueSkills().update(user_rating, task_rating, is_ok, time, expectedTime)
-        diff = new_user_rating - user_rating
-        user_rating += diff
-        task_rating -= diff
-        return int(DB_CONST * user_rating), int(DB_CONST * task_rating), int(DB_CONST * diff)
+        diff = int(DB_CONST * (new_user_rating - user_rating))
+        user_rating = int(DB_CONST * user_rating + diff)
+        task_rating = int(DB_CONST * task_rating - diff)
+        return user_rating, task_rating, diff
+
+
+user_df = pd.DataFrame({'id': [1, 2, 3], 'rating': [2000, 1000, 3000]})
+task_df = pd.DataFrame({'id': [1, 2, 3], 'rating': [1500, 700, 2800]})
+sol_df = pd.DataFrame({'id': [], 'user_id': [], 'task_id': []})
+
+for i in range(len(user_df)):
+    for j in range(len(task_df)):
+        print(UpdateData.update_rating(
+            user_df.iloc[i].id,
+            task_df.iloc[j].id,
+            0,
+            True,
+            user_df,
+            task_df,
+            sol_df
+        ))
